41fc1d46229aa994a6054605cdcee2e0fd94359c
mark <mhlrsym9@comcast.net>
2021-10-13 15:41:20 -0400

    Document the code necessary to allow the TinyMCE component to get information into the Elm system.

--- src/tinymce.js
+++ src/tinymce.js
@@ -1,17 +1,25 @@
 import {app} from "./index.js";
 
+// TinyMCE setup attribute needs to refer to a function in the global namespace, outside
+// of any module. Therefore, create the function as a property of the global window object...
 window.setupEditor = function(editor) {
     editor.on('dirty', handleDirty);
 };
+
+// Since the just created function has an empty string for a name, give it a name
+// using the defineProperty function.
 Object.defineProperty(window.setupEditor, 'name', {
     value: 'setupEditor',
     writable: false
 });
 
+// Now we finally have a string we can use in the setup attribute that TinyMCE can
+// find and call :)
 function setupEditorName() {
     return window.setupEditor.name;
 }
 
+// Fire off the dirtyReceived port by sending true
 function handleDirty() {
     app.ports.dirtyReceived.send(true);
 }
